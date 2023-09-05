1d34e2b46ed8f694042ccd616bf3b6e54e0e7b36
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2023-09-04 23:50:21 -0600

    Implementada version 1.0

--- dudo-game/src/components/App.jsx
+++ dudo-game/src/components/App.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import DiceComponent from './diceComponent.jsx';
+
+import {
+  IconDice1,
+  IconDice2,
+  IconDice3,
+  IconDice4,
+  IconDice5,
+  IconDice6,
+  IconDice1Filled,
+  IconDice2Filled,
+  IconDice3Filled,
+  IconDice4Filled,
+  IconDice5Filled,
+  IconDice6Filled,
+} from '@tabler/icons-react';
 
 import '../styles/app.css';
 
 function App() {
-  // Estado para mantener un registro de los números de los dados
+
   const [diceNumbers, setDiceNumbers] = useState([0, 0, 0, 0, 0, 0]);
+  const [numberOccurrences, setNumberOccurrences] = useState({});
 
   // Función para actualizar el número de un dado específico
   const updateDiceNumber = (index, number) => {
@@ -14,6 +30,30 @@ function App() {
     newDiceNumbers[index] = number;
     setDiceNumbers(newDiceNumbers);
   };
+
+  const getDiceIcon = (number, occurrences) => {
+    const icons = {
+      1: occurrences ? IconDice1Filled : IconDice1,
+      2: occurrences ? IconDice2Filled : IconDice2,
+      3: occurrences ? IconDice3Filled : IconDice3,
+      4: occurrences ? IconDice4Filled : IconDice4,
+      5: occurrences ? IconDice5Filled : IconDice5,
+      6: occurrences ? IconDice6Filled : IconDice6,
+    };
+    const Icon = icons[number];
+    return <Icon />;
+  };
+
+  // UseEffect para contar las ocurrencias de cada número
+  useEffect(() => {
+    const occurrences = {};
+    diceNumbers.forEach((number) => {
+      if (number !== 0) {
+        occurrences[number] = occurrences[number] ? occurrences[number] + 1 : 1;
+      }
+    });
+    setNumberOccurrences(occurrences);
+  }, [diceNumbers]);
 
   return (
     <>
@@ -23,10 +63,12 @@ function App() {
         ))}
       </div>
       <div className="dice-numbers">
-        <h2>Números de los dados:</h2>
+        <h3>Ocurrencias por numero</h3>
         <ul>
-          {diceNumbers.map((number, index) => (
-            <li key={index}>Dado {index + 1}: {number}</li>
+          {[1, 2, 3, 4, 5, 6].map((number) => (
+            <li key={number}>
+              {getDiceIcon(number, numberOccurrences[number])} {numberOccurrences[number] || 0}
+            </li>
           ))}
         </ul>
       </div>
